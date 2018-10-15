6c3ebeac824f2d58a892507cd40a89e12642fa06
lijjLyb <lljj2277@qq.com>
2018-10-16 01:19:47 +0800

    fix: 无法启动命令行

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-import { start } from "./liwork"
+import { CommandGetter, CommandLike } from "./lib/CMDGetter";
+import MdBase from "./lib/ModeBase";
+
+async function start() {
+    const command = new CommandGetter(process.argv.slice(2));
+    let Mod = await import("./mods/" + command.getMain());
+    const mod: new (cmdLike: CommandLike[]) => MdBase = Mod.default;
+    let runMod = new mod(command.getSub());
+    if (runMod) {
+        await runMod.run();
+    }
+    else {
+        throw `主命令${command.getMain()}不存在`
+    }
+}
 
 start();
--- src/lib/CMDGetter.ts
+++ src/lib/CMDGetter.ts
@@ -33,7 +33,7 @@ export class CommandGetter {
     ) { }
 
     getMain() {
-        return this.argv[0].substr(1);
+        return this.argv[0];
     }
 
     getSub() {
